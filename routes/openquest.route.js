7906f4b96e044eb396132d04e8e6fb693504e3f4
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-11-16 11:06:54 +0100

    reorganisation des routes users/admin

--- routes/openquest.route.js
+++ routes/openquest.route.js
@@ -4,8 +4,18 @@ const userController = require('../controllers/user.controller');
 const checkTokenMiddleware = require('../controllers/jwt.controller');
 const openquestController = require("../controllers/openquest.controller");
 
+//routes ADMIN
 //POST
-router.post('/magic/:dataName',checkTokenMiddleware.checkTokenAdmin, openquestController.register); //utilisé pour remplir la base de données
+router.post('admin/magic/:dataName',checkTokenMiddleware.checkTokenAdmin, openquestController.register); //utilisé pour remplir la base de données
+
+//GET
+
+//DELETE
+router.delete('admin/character/:id',checkTokenMiddleware.checkTokenAdmin, openquestController.deleteCharacter )
+
+
+//routes USER
+//POST
 router.post('/character/',checkTokenMiddleware.checkToken, openquestController.createCharacter);
 
 //GET
@@ -14,6 +24,5 @@ router.get('/get/:type', openquestController.getFromDatabase);
 
 //DELETE
 router.delete('/character/:id',checkTokenMiddleware.checkToken, openquestController.deleteCharacter )
-router.delete('/character/delete/:id',checkTokenMiddleware.checkTokenAdmin, openquestController.deleteCharacter )
 
 module.exports = router;
