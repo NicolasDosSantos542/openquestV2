919611eb185a9647567009529f8bc231c141f1f0
Nico <nicolas.de-meyere-dos-santos@epitech.eu>
2022-02-10 15:48:04 +0100

    datasetter is now complete

--- dataSetter/dataSetter.js
+++ dataSetter/dataSetter.js
@@ -1,16 +1,26 @@
 spellsData = require('./datas/spells_data.json');
-skillData = require('./datas/skills_data.json');
 armorData = require('./datas/armors_data.json');
 attributeData = require('./datas/attributes_data.json');
 caracData = require('./datas/carac_data.json');
-weaponsData = require('./datas/weapons_data.json');
 dailyData = require('./datas/daily_data.json');
+skillData = require('./datas/skills_data.json');
+weaponsData = require('./datas/weapons_data.json');
 
 const res = require("express");
 const index = require("../server");
 const {log} = require("debug");
 const openquestService = require("../services/openquest.service");
 
+let array = {
+
+"spell":spellsData,
+"armor":armorData,
+"attribute":attributeData,
+"carac":caracData,
+"daily":dailyData,
+"skill":skillData,
+"weapon":weaponsData
+}
 
 async function addData(data, type) {
     for (const element of data) {
@@ -40,5 +50,10 @@ async function addData(data, type) {
 //         }
 //
 // }
+for (const property in array) {
 
-addData(armorData, "armor").then(r => console.log("fin" )).then(r => process.exit(1))
+
+
+    addData(array[property], property).then(r => console.log("fin" )).then(r => process.exit(1))
+
+  }
